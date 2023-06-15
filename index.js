b6371661af4aaab168ce74ed82ee8a0d4da5e49d
SSashinn <sayedayaan226@gmail.com>
2023-06-15 20:04:29 +0530

    Added a button which inputs size.

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const mainBox = document.querySelector('.main-box');
-let size;
-while (true){
-    size = prompt("NO OF GRIDS");
-    if (size>100 && size<0){
-        alert("No of grids should be less than 100.");}
-    else{
-        break;
+let size = 16;
+grids(size);
+const button = document.querySelector('#button').onclick = sizes;
+
+function sizes(){
+    while (true){
+        size = prompt("NO OF GRIDS");
+        if (size>100 || size<1){
+            alert("Number of grids should be less than 100 and it should not be negative.")}
+        else if (size.length==0){
+            alert("Type some value.")
+        }
+        else{
+            document.querySelector('.main-box').textContent = '';
+            grids(size);
+            return 1;
+        }
     }
 }
 
 
-for (let j=0;j<size;j++){
-    const gridColumn = document.createElement('div');
-    gridColumn.classList.add('grid-column');
-    mainBox.appendChild(gridColumn)
-    for (let i = 0;i<size; i++){
 
-        const grid = document.createElement('div');
-        grid.classList.add('grid')
-        gridColumn.appendChild(grid);
-    }
+function grids(size){
+    for (let j=0;j<size;j++){
+        const gridColumn = document.createElement('div');
+        gridColumn.classList.add('grid-column');
+        mainBox.appendChild(gridColumn)
+        for (let i = 0;i<size; i++){
     
+            const grid = document.createElement('div');
+            grid.classList.add('grid')
+            gridColumn.appendChild(grid);
+        } 
+    }
 }
+
 
